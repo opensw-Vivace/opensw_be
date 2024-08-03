d78229af2db10d913ebdbf96b6ea4b2698332598
SimonCreater <112795666+SimonCreater@users.noreply.github.com>
2024-08-03 19:47:04 +0900

    ArtifactEntity

---
+++ src/main/java/com/vivace/opensw/entity/ArtifactEntity.java
@@ -0,0 +1,30 @@
+package com.vivace.opensw.entity;
+
+import jakarta.persistence.*;
+
+import javax.annotation.processing.Generated;
+import java.util.Date;
+
+@Entity
+public class ArtifactEntity extends BaseEntity{
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column
+  private Long id;
+  @Column
+  private String title;
+  @Column
+  private String subtitle;
+  @Column
+  private String status;
+
+  @Column
+  private Date deadline;
+  @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.REMOVE)
+  @JoinColumn(name = "project_id")
+  ProjectEntity projectEntity;
+  @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.REMOVE)
+  @JoinColumn(name = "artifactType_id")
+  ArtifactTypeEntity artifactTypeEntity;
+
+}
