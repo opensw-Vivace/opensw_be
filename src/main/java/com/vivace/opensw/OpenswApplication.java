84322720598db783f0a4ba3e8b2acef52a33bfb8
SimonCreater <112795666+SimonCreater@users.noreply.github.com>
2024-08-03 18:22:18 +0900

    dd

---
+++ src/main/java/com/vivace/opensw/OpenswApplication.java
@@ -0,0 +1,13 @@
+package com.vivace.opensw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenswApplication.class, args);
+	}
+
+}
