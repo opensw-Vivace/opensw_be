f59b8c28fcc3a9e3a42dce1321cc40a86de713d0
SimonCreater <112795666+SimonCreater@users.noreply.github.com>
2024-08-10 23:31:37 +0900

    todolist

--- src/main/java/com/vivace/opensw/dto/ProjectListView.java
+++ src/main/java/com/vivace/opensw/dto/ProjectListView.java
@@ -1,9 +1,11 @@
 package com.vivace.opensw.dto;
 
 import com.vivace.opensw.entity.Project;
+import com.vivace.opensw.entity.ToDo;
 import lombok.Getter;
 
 import java.util.Date;
+import java.util.List;
 
 @Getter
 public class ProjectListView {
@@ -11,12 +13,14 @@ public class ProjectListView {
   private final String title;
   private final String description;
   private final Date deadline;
+  private final List<ToDo> todo;
 
   public ProjectListView(Project projectEntity){
     this.id=projectEntity.getId();
     this.title=projectEntity.getTitle();
     this.description=projectEntity.getDescription();
     this.deadline=projectEntity.getDeadline();
+    this.todo=projectEntity.getToDoList();
   }
 
 }
