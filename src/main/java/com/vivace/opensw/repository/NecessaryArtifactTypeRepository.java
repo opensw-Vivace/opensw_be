57648592a004fe69e7b65949cf25faccdcfda7dd
dubae <goat2001@naver.com>
2024-08-19 02:42:33 +0900

    feat: 필수 산출물 설정 API 구현

--- src/main/java/com/vivace/opensw/repository/NecessaryArtifactTypeRepository.java
+++ src/main/java/com/vivace/opensw/repository/NecessaryArtifactTypeRepository.java
@@ -4,4 +4,5 @@ import com.vivace.opensw.entity.NecessaryArtifactType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NecessaryArtifactTypeRepository extends JpaRepository<NecessaryArtifactType, Long> {
+    public void deleteByProjectIdAndArtifactTypeId(Long project_id, Long artifactType_id);
 }
