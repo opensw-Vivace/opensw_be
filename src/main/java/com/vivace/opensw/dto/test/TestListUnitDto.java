4efbab017523b7aad86639834f88fbd90ef99a5e
seohyun-lee <applemint2003@gmail.com>
2024-08-20 23:03:17 +0900

    fix: TestListUnitDto에 id 전송

--- src/main/java/com/vivace/opensw/dto/test/TestListUnitDto.java
+++ src/main/java/com/vivace/opensw/dto/test/TestListUnitDto.java
@@ -5,12 +5,14 @@ import com.vivace.opensw.entity.Test;
 import java.time.LocalDateTime;
 
 public record TestListUnitDto(
+        Long id,
         String title,
         String name,
         LocalDateTime created_at
 ) {
     public static TestListUnitDto from(Test test) {
         return new TestListUnitDto(
+                test.getId(),
                 test.getTitle(),
                 test.getMember().getName(),
                 test.getCreatedAt()
