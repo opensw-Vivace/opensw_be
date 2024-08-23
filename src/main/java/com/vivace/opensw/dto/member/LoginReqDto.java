cc7c699558b4dd8f72e0de2ac4beff78fe649034
seohyun-lee <applemint2003@gmail.com>
2024-08-23 20:36:51 +0900

    refactor: dto 네이밍 및 디렉토리 구조 변경

--- src/main/java/com/vivace/opensw/dto/member/LoginReqDto.java
+++ src/main/java/com/vivace/opensw/dto/member/LoginReqDto.java
@@ -1,8 +1,8 @@
-package com.vivace.opensw.dto.member.request;
+package com.vivace.opensw.dto.member;
 
 import jakarta.validation.constraints.NotEmpty;
 
-public record LoginRequestDto(
+public record LoginReqDto(
         @NotEmpty(message = "INVALID_INPUT_VALUE-이메일을 입력하세요.")
         String email,
         @NotEmpty(message = "INVALID_INPUT_VALUE-비밀번호를 입력하세요.")
