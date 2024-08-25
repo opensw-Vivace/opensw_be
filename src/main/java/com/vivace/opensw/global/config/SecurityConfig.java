f25983be61f583fb96221760e02b0cb7570fcf3a
seohyun-lee <applemint2003@gmail.com>
2024-08-25 11:53:43 +0900

    fix: Security Config에 FE 배포 주소 추가

--- src/main/java/com/vivace/opensw/global/config/SecurityConfig.java
+++ src/main/java/com/vivace/opensw/global/config/SecurityConfig.java
@@ -50,7 +50,8 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:3000",
-                "https://localhost:3000"));
+                "https://localhost:3000",
+                "https://api.devhelp.p-e.kr"));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"));
         configuration.addAllowedHeader("*");
