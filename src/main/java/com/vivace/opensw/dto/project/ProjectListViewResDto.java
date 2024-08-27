7a1c0113d16921b63415cc35643845973545108d
SimonCreater <112795666+SimonCreater@users.noreply.github.com>
2024-08-27 20:41:23 +0900

    feat:projectModify

--- src/main/java/com/vivace/opensw/dto/project/ProjectListViewResDto.java
+++ src/main/java/com/vivace/opensw/dto/project/ProjectListViewResDto.java
@@ -1,21 +1,28 @@
 package com.vivace.opensw.dto.project;
 
 import com.vivace.opensw.entity.Project;
+import com.vivace.opensw.model.DocsStatus;
+import com.vivace.opensw.model.ProjectStatus;
 
 import java.time.LocalDate;
 
 public record ProjectListViewResDto(
-        Long id,
+        Long project_id,
         String title,
         String description,
-        LocalDate deadline
+        LocalDate deadline,
+        ProjectStatus status,
+        String team_name
 ) {
     public static ProjectListViewResDto from(Project project) {
         return new ProjectListViewResDto(
                 project.getId(),
                 project.getTitle(),
                 project.getDescription(),
-                project.getDeadline()
+                project.getDeadline(),
+                project.getStatus(),
+                project.getTeamName()
+
         );
     }
 }
